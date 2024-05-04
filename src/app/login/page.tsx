8a57a6f871efe83b5b7c9c2efba4db6fd877e5bb
Behnam Hamidi <eslp91@hotmail.com>
2024-05-04 12:37:14 +0330

    checkout

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,11 +1,22 @@
+"use client";
 import "./login.css";
+import Link from "next/link";
+import React from "react";
+import { useRouter } from "next/navigation";
+import axios from "axios";
 
 export default function LoginPage() {
+  const [user, setUser] = React.useState({
+    email: "",
+    password: "",
+    username: "",
+  });
+
   return (
     <div className="login">
       <form className="login-form">
         <h1>Login Form</h1>
-        <input type="email" placeholder="Enter your email eg. john@email.com" />
+        <input type="email" placeholder="Email eg. john@email.com" />
         <input type="password" placeholder="Enter your password" />
         <button type="submit">Loign</button>
       </form>
--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,14 +1,41 @@
+"use client";
 import "./register.css";
+import Link from "next/link";
+import React from "react";
+import { useRouter } from "next/navigation";
+import axios from "axios";
 
 export default function Register() {
+  const [user, setUser] = React.useState({
+    email: "",
+    password: "",
+    username: "",
+  });
   return (
     <div className="register">
       <form className="register-form">
         <h1>Register Form</h1>
-        <input type="text" placeholder="Enter your name e.g. John Doe" />
-        <input type="email" placeholder="Enter your email eg. john@email.com" />
-        <input type="password" placeholder="Enter your password" />
-        <input type="password" placeholder="Confirm your" />
+        <input
+          type="text"
+          placeholder="Username e.g. johndoe"
+          id="username"
+          value={user.username}
+          onChange={(e) => setUser({ ...user, username: e.target.value })}
+        />
+        <input
+          type="email"
+          placeholder="Email eg. john@email.com"
+          id="email"
+          value={user.email}
+          onChange={(e) => setUser({ ...user, email: e.target.value })}
+        />
+        <input
+          type="password"
+          placeholder="Enter your password"
+          id="password"
+          value={user.password}
+          onChange={(e) => setUser({ ...user, password: e.target.value })}
+        />
         <button type="submit">Register</button>
       </form>
     </div>
